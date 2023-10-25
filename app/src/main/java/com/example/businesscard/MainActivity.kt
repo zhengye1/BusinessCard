d2a7be1d8c4d019d733b8130f26f79f8c6d36097
Vincent Zheng <vincentcheng787@gmail.com>
2023-10-25 18:56:14 -0400

    First try

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,12 +3,22 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
@@ -29,6 +39,60 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Composable
+fun BusinessApp() {
+    Column(Modifier.fillMaxWidth()) {
+        BusinessCard(
+            name = stringResource(R.string.name),
+            title = stringResource(R.string.title),
+            backgroundColor = Color(0xFF3ddc84),
+            avatar = painterResource(R.drawable.android_logo)
+
+        )
+    }
+    Column(Modifier.fillMaxWidth()) {
+        ContactCard(
+            bilibiliLink = stringResource(id = R.string.biliLink),
+            twitterLink = stringResource(id = R.string.twiter),
+            backgroundColor = Color(0xFF3ddc84),
+            email = stringResource(id = R.string.email)
+        )
+    }
+}
+
+@Composable
+private fun BusinessCard(
+    name: String,
+    title: String,
+    backgroundColor: Color,
+    avatar: Painter,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+            .background(backgroundColor),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = name,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+private fun ContactCard(
+    bilibiliLink: String,
+    twitterLink: String,
+    email: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -41,6 +105,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BusinessCardTheme {
-        Greeting("Vincent")
+        BusinessApp()
     }
 }
