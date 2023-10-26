eb5ec4458ebf7e27c4bda1c01ffb9211d624941d
Vincent Zheng <vincentcheng787@gmail.com>
2023-10-26 13:02:28 -0400

    Business card finished, something can try will be create bilibili link icon and twitter icon to replace with share icon

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,23 +3,34 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,9 +41,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xFFD2E8D4)
                 ) {
-                    Greeting("Vincent")
+                    BusinessApp()
                 }
             }
         }
@@ -42,69 +53,85 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessApp() {
-    Column(Modifier.fillMaxWidth()) {
-        BusinessCard(
-            name = stringResource(R.string.name),
-            title = stringResource(R.string.title),
-            backgroundColor = Color(0xFF3ddc84),
-            avatar = painterResource(R.drawable.android_logo)
-
-        )
-    }
-    Column(Modifier.fillMaxWidth()) {
-        ContactCard(
-            bilibiliLink = stringResource(id = R.string.biliLink),
-            twitterLink = stringResource(id = R.string.twiter),
-            backgroundColor = Color(0xFF3ddc84),
-            email = stringResource(id = R.string.email)
-        )
-    }
-}
-
-@Composable
-private fun BusinessCard(
-    name: String,
-    title: String,
-    backgroundColor: Color,
-    avatar: Painter,
-    modifier: Modifier = Modifier
-) {
     Column(
-        modifier = modifier.fillMaxSize()
-            .background(backgroundColor),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var name = stringResource(R.string.name)
+        var title = stringResource(R.string.title)
+        var avatar = painterResource(R.drawable.android_logo)
+        var bilibiliLink = stringResource(id = R.string.biliLink)
+        var twitterLink = stringResource(id = R.string.twiter)
+        var email = stringResource(id = R.string.email)
+
+        Image(
+            painter = avatar,
+            contentDescription = null,
+            Modifier
+                .fillMaxWidth(0.3f)
+                .background((Color(0xFF073042)))
+        )
         Text(
             text = name,
-            fontWeight = FontWeight.Bold
+            fontSize = 50.sp,
+            color = Color(0xFF1b211c),
+            fontFamily = FontFamily.SansSerif
         )
+        Text(
+            text = title,
+            fontSize = 25.sp,
+            color = Color(0xFF5da17b),
+            fontWeight = Bold,
+            fontFamily = FontFamily.SansSerif
+        )
+        Spacer(modifier = Modifier.padding(bottom = 200.dp))
+        Divider(modifier = Modifier.fillMaxWidth(), color = Color(0xFF4F6C79))
+        ContactRow(
+            text = email,
+            icon = Icons.Rounded.Email
+        )
+        Divider(modifier = Modifier.fillMaxWidth(), color = Color(0xFF4F6C79))
+        ContactRow(
+            text = bilibiliLink,
+            icon = Icons.Rounded.Share
+        )
+        Divider(modifier = Modifier.fillMaxWidth(), color = Color(0xFF4F6C79))
+        ContactRow(
+            text = twitterLink,
+            icon = Icons.Rounded.Share
+        )
+    }
+
+}
+
+@Composable
+fun ContactRow(
+    text: String,
+    icon: ImageVector
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center, modifier = Modifier.padding(16.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = Color(0xFF187949),
+            modifier = Modifier.weight(1f)
+        )
+        Text(text = text, color = Color.Black, modifier = Modifier.weight(3f))
     }
 }
 
-@Composable
-private fun ContactCard(
-    bilibiliLink: String,
-    twitterLink: String,
-    email: String,
-    backgroundColor: Color,
-    modifier: Modifier = Modifier
-) {
-
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BusinessCardTheme {
-        BusinessApp()
+fun BusniessCardPreview() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFFD2E8D4)
+    ) {
+        BusinessCardTheme {
+            BusinessApp()
+        }
     }
 }
